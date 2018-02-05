1835b6452706a058baed2014b5dc9e0c494bc347
Dean151 <thomas.durand.verjat@gmail.com>
2018-02-05 07:36:32 +0100

    Useful constructors added to DateTime prototype

--- src/datetime.js
+++ src/datetime.js
@@ -1,8 +1,23 @@
 'use strict'
 
 function DateTime(hours, minutes) {
-  this._hours = hours;
-  this._minutes = minutes;
+  if (hours === undefined && minutes === undefined) {
+    var now = new Date();
+    this._hours = now.getHours();
+    this._minutes = now.getMinutes();
+  }
+  else {
+    if (hours >= 0 && hours % 24 === hours) {
+      this._hours = hours;
+    }
+    if (minutes >= 0 && minutes % 60 === minutes) {
+      this._minutes = minutes;
+    }
+  }
+
+  if (this._hours === undefined || this._minutes === undefined) {
+    throw 'hours or minutes parameters are unvalid';
+  }
 }
 
 DateTime.prototype.numberOfSeconds = function() {
--- tests/datetime-test.js
+++ tests/datetime-test.js
@@ -5,8 +5,26 @@ var expect = chai.expect; // we are using the "expect" style of Chai
 
 var DateTime = require('./../src/datetime');
 
-// numberOfSeconds()
 describe('DateTime', function() {
+
+  // Default constructor
+  it('DateTime() should be "now" datetime', function() {
+    var date = new DateTime();
+    var now = new Date();
+    expect(date._hours).to.equal(now.getHours());
+    expect(date._minutes).to.equal(now.getMinutes());
+  });
+
+  // Unvalid constructions
+  it('DateTime() with unvalid parameters should throw', function() {
+    expect(() => new DateTime(42, 0)).to.throw();  
+    expect(() => new DateTime(1, 61)).to.throw();  
+    expect(() => new DateTime(-2, 0)).to.throw();  
+    expect(() => new DateTime(5, -6)).to.throw();  
+    expect(() => new DateTime(2)).to.throw();  
+  });
+
+  // numberOfSeconds()
   it('numberOfSeconds() should return 420 for midnight (17h is 0)', function() {
     var midnight = new DateTime(0, 0);
     expect(midnight.numberOfSeconds()).to.equal(420);
