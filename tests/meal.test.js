a36d954dd7a1cfdfdb80f0b96ac6d1ca286871ce
Dean151 <thomas.durand.verjat@gmail.com>
2018-02-07 09:01:43 +0100

    Added test with non-default hour offset

--- tests/meal.test.js
+++ tests/meal.test.js
@@ -48,6 +48,7 @@ test("Meal() with unvalid parameters should throw", () => {
 
 // buffered()
 test("buffered() should send the correct binary value", () => {
+  expect(new Meal(0, 0, 5).buffered(0,0).toString("hex")).toBe("00000005");
   expect(new Meal(8, 30, 8).buffered().toString("hex")).toBe("03a20008");
   expect(new Meal(12, 30, 10).buffered().toString("hex")).toBe("0492000a");
   expect(new Meal(16, 0, 11).buffered().toString("hex")).toBe("0564000b");
