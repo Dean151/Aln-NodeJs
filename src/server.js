cee16e0823c07fc73b1fb6f404b7456b9cd85e57
Thomas Durand <thomas.durand.verjat@gmail.com>
2018-12-28 14:39:55 +0100

    Added CSRF protection

--- src/server.js
+++ src/server.js
@@ -23,6 +23,7 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const MySQLStore = require('express-mysql-session')(session);
 const bcrypt = require('bcrypt');
+const csrf = require('csurf')
 
 const Quantity = require("./models/quantity");
 const Meal = require("./models/meal");
@@ -83,9 +84,45 @@ class Server {
       res.json({ success: false, error: err.toString() });
     });
 
+    let requiresNotLoggedIn = (req, res, next) => {
+      if (req.session && req.session.user) {
+        res.status(401);
+        res.json({ success: false, error: 'Already logged-in as ' + req.session.user.email });
+      } else {
+        next();
+      }
+    };
+
     /** AUTHENTICATION MECHANISMS **/
 
-    api.post('/user/login', Server.requiresNotLoggedIn, (req, res, next) => {
+    api.post('/user/register', requiresNotLoggedIn, (req, res, next) => {
+      if (!req.body.email || !req.body.password) {
+        throw 'Missing email or password';
+      }
+
+      // Check if the user already exists
+      database.getUser(req.body.email, (user) => {
+        if (typeof user !== 'undefined') {
+          res.status(401);
+          res.json({ success: false, error: 'Email already in use' });
+        } else {
+          // TODO create user and register it
+
+          database.getUser(req.body.email, (user) => {
+            if (typeof user === 'undefined') {
+              res.status((500));
+              res.json({ success: false, error: 'Registration failed' });
+            } else {
+              // Auto-connexion
+              req.session.user = user.jsoned();
+              res.json({ success: true, user: req.session.user, token: req.csrfToken() });
+            }
+          });
+        }
+      });
+    });
+
+    api.post('/user/login', requiresNotLoggedIn, (req, res, next) => {
       if (!req.body.email || !req.body.password) {
         throw 'Missing email or password';
       }
@@ -101,42 +138,28 @@ class Server {
               res.json({ success: false, error: 'Wrong email/password' });
             } else {
               req.session.user = user.jsoned();
-              res.json({ success: true, user: req.session.user });
+              res.json({ success: true, user: req.session.user, token: req.csrfToken() });
             }
           });
         }
       });
     });
 
-    api.post('/user/register', Server.requiresNotLoggedIn, (req, res, next) => {
-      if (!req.body.email || !req.body.password) {
-        throw 'Missing email or password';
+    // Every endpoint below requires login-in
+    api.use((req, res, next) => {
+      if (!req.session || !req.session.user) {
+        res.status(403);
+        res.json({ success: false, error: 'Not logged-in' });
+      } else {
+        next();
       }
-
-      // Check if the user already exists
-      database.getUser(req.body.email, (user) => {
-        if (typeof user !== 'undefined') {
-          res.status(401);
-          res.json({ success: false, error: 'Email already in use' });
-        } else {
-          // TODO create user and register it
-
-          database.getUser(req.body.email, (user) => {
-            if (typeof user === 'undefined') {
-              res.status(500);
-              res.json({ success: false, error: 'Registration failed' });
-            } else {
-              // Auto-connexion
-              req.session.user = user.jsoned();
-              res.json({ success: true, user: req.session.user });
-            }
-          });
-        }
-      });
     });
+
+    // ... And CSRF protection
+    api.use(csrf({}));
 
     // Logging out
-    api.post('/user/logout', Server.requiresLogin, (req, res, next) => {
+    api.post('/user/logout', (req, res, next) => {
       // delete session object
       req.session.destroy(function(err) {
         if(err) {
@@ -149,12 +172,23 @@ class Server {
 
     /** FEEDER HANDLING **/
 
-    api.route('/feeder/status').post(Server.requiresLogin, Server.checkFeederAssociation, (req, res) => {
+    // We now need to check feeder association
+    api.use((req, res, next) => {
+      if (typeof req.body.identifier === 'undefined') {
+        res.status(500);
+        res.json({ success: false, error: 'No feeder identifier given' });
+      } else {
+        // TODO: check feeder <-> user association
+        next();
+      }
+    });
+
+    api.route('/feeder/status').post((req, res) => {
       let feeders = feederCoordinator.getFeeder(req.body.identifier);
       res.json(feeders);
     });
 
-    api.route('/feeder/feed').put(Server.requiresLogin, Server.checkFeederAssociation, (req, res) => {
+    api.route('/feeder/feed').put((req, res) => {
       let quantity = new Quantity(req.body.quantity);
       feederCoordinator.feedNow(req.body.identifier, quantity, (msg) => {
         if (msg !== 'success') {
@@ -164,7 +198,7 @@ class Server {
       });
     });
 
-    api.route('/feeder/quantity').put(Server.requiresLogin, Server.checkFeederAssociation, (req, res) => {
+    api.route('/feeder/quantity').put((req, res) => {
       let quantity = new Quantity(req.body.quantity);
       feederCoordinator.setDefaultQuantity(req.body.identifier, quantity, (msg) => {
         if (msg !== 'success') {
@@ -175,7 +209,7 @@ class Server {
     });
 
     api.route('/feeder/planning')
-      .post(Server.requiresLogin, Server.checkFeederAssociation, (req, res) => {
+      .post((req, res) => {
         // Fetch the planning if it's exists
         database.getCurrentPlanning(req.body.identifier, (planning) => {
           if (typeof planning === 'undefined') {
@@ -185,7 +219,7 @@ class Server {
           res.json({ success: true, meals: planning.jsoned() });
         });
       })
-      .put(Server.requiresLogin, Server.checkFeederAssociation, (req, res) => {
+      .put((req, res) => {
         let meals = req.body.meals.map((obj) => { return new Meal(obj.time, obj.quantity, obj.enabled); });
         let planning = new Planning(meals);
         feederCoordinator.setPlanning(req.body.identifier, planning, (msg) => {
@@ -197,34 +231,6 @@ class Server {
       });
 
     return api;
-  }
-
-  static requiresNotLoggedIn (req, res, next) {
-    if (req.session && req.session.user) {
-      res.status(401);
-      res.json({ success: false, error: 'Already logged-in as ' + req.session.user.email });
-    } else {
-      next();
-    }
-  }
-
-  static requiresLogin (req, res, next) {
-    if (!req.session || !req.session.user) {
-      res.status(403);
-      res.json({ success: false, error: 'Not logged-in' });
-    } else {
-      next();
-    }
-  }
-
-  static checkFeederAssociation (req, res, next) {
-    if (typeof req.body.identifier === 'undefined') {
-      res.status(500);
-      res.json({ success: false, error: 'No feeder identifier given' });
-    } else {
-      // TODO: check feeder <-> user association
-      next();
-    }
   }
 }
 
