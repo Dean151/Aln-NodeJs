30bf8dcfefa813d0644eb8158c1270015f752b4f
Thomas Durand <thomas.durand.verjat@gmail.com>
2019-01-02 11:12:10 +0100

    Fixing again the login API

--- src/models/user.js
+++ src/models/user.js
@@ -16,7 +16,7 @@ CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 "use strict";
 
-const CryptoHelper = require('./crypto-helper');
+const CryptoHelper = require('../crypto-helper');
 
 class User {
 
